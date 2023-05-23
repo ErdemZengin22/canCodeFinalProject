5fc2444a4ec304f89df4b5dad8c9e10b2b48ef40
Ridygo <erdmzengin@gmail.com>
2023-05-23 14:41:02 -0400

    Basic search function

---
+++ js/api.js
@@ -0,0 +1,5 @@
+(function (){
+  const API_KEY = '20fa8e9fd0c7311256a7409b62232ce1';
+
+  window.API_KEY = API_KEY;
+})();
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function(){
-  const API_KEY = '20fa8e9fd0c7311256a7409b62232ce1';
   const latestMovies = document.querySelector('#latest-movies');
   const upcomingMovies = document.querySelector('#upcoming-movies');
+  const searchInput = document.querySelector('#movie-search');
 
   const topMovies = async () => {
     try{
@@ -80,6 +80,62 @@
       console.log(error);
     }
   }
+  const searchMovies = async () => {
+    const userInput = searchInput.value;
+    try{
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&api_key=${API_KEY}&query=${userInput}`);
+      const data = await response.json();
+      let movies = data.results;
+
+    let counter = 0; 
+    
+    const searchContainer = document.querySelector('.nav-item.search')
+    const searchResults = document.createElement('div');
+    searchResults.id = 'search-results';
+    searchContainer.appendChild(searchResults);
+
+    movies = movies.filter(movie => movie.poster_path);
+
+    movies.forEach((movie) => {
+      const movieItem = document.createElement('div');
+      movieItem.classList.add('movie-item');
+      movieItem.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${movie.backdrop_path}')`;
+      const movieItemBackgroundFilter = document.createElement('div');
+      movieItemBackgroundFilter.classList.add('movie-item-background-filter');
+      const movieItemInnerContainer = document.createElement('div');
+      movieItemInnerContainer.classList.add('movie-item-inner-container');
+      movieItem.appendChild(movieItemInnerContainer);
+      const leftContainer = document.createElement('div');
+      leftContainer.classList.add('left-container');
+      movieItemInnerContainer.appendChild(leftContainer);
+      const movieImg = document.createElement('img');
+      movieImg.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+      leftContainer.appendChild(movieImg);
+      const rightContainer = document.createElement('div');
+      rightContainer.classList.add('right-container');
+      movieItemInnerContainer.appendChild(rightContainer);
+      const movieTitle = document.createElement('h2');
+      movieTitle.innerText = movie.original_title;
+      rightContainer.appendChild(movieTitle);
+      const movieOverview = document.createElement('p');
+      const overviewText = movie.overview;
+      movieOverview.innerText = overviewText.substring(0, 250) + '...';
+      rightContainer.appendChild(movieOverview);
+      movieItem.insertAdjacentElement('beforeend', movieItemBackgroundFilter);
+      searchResults.appendChild(movieItem);
+    });
+    window.addEventListener('click', (event) => {
+      if (searchResults.parentNode && !searchResults.contains(event.target)) {
+        searchResults.parentNode.removeChild(searchResults);
+        searchInput.value = '';
+      }
+    });
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  searchInput.addEventListener('input', searchMovies);
   topMovies();
   comingMovies();
 })();
