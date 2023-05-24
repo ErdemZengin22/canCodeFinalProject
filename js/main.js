dec8d19f7c4415f18a023264f1f0ede8afe0ee93
Ridygo <erdmzengin@gmail.com>
2023-05-23 22:54:55 -0400

    Added Top 4 People feature

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function(){
   topMovies();
   comingMovies();
+  topPeople();
 })();
---
+++ js/top-people.js
@@ -0,0 +1,30 @@
+(function(){
+  const topPeopleSection = document.querySelector('#top-people');
+  const topPeople = async () => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/trending/person/day?language=en-US&api_key=${API_KEY}`);
+      const data = await response.json();
+      const people = data.results.splice(0, 4);
+
+      people.forEach(async person => {
+        const personItem = document.createElement('div');
+        personItem.classList.add('movie-item');
+        personItem.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${person.profile_path}')`;
+        const personItemBackgroundFilter = document.createElement('div');
+        personItemBackgroundFilter.classList.add('movie-item-background-filter');
+        const personImg = document.createElement('img');
+        personImg.src = `https://image.tmdb.org/t/p/w500${person.profile_path}`;
+        personItem.appendChild(personImg);
+        const personTitle = document.createElement('h2');
+        personTitle.innerText = person.name;
+        personItem.appendChild(personTitle);
+        personItem.insertAdjacentElement('beforeend', personItemBackgroundFilter);
+        topPeopleSection.appendChild(personItem);
+      });
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+  window.topPeople = topPeople;
+})();
